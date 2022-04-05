6970394bd779085fd3283ffb04476b70be90d9ea
Stephen Koch <kochsj@gmail.com>
2022-04-05 09:52:59 -0700

    Initial Commit

---
+++ WineRecommender/WineRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineRecommenderApp.swift
+//  WineRecommender
+//
+//  Created by Stephen Koch on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WineRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
